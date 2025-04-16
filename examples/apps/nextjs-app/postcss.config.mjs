1901474b7bd086e68cdf13682a3a3e25037fa858
Sébastien Vanvelthem <belgattitude@gmail.com>
2025-04-16 22:29:02 +0200

    feat: test glide

--- examples/apps/nextjs-app/postcss.config.mjs
+++ examples/apps/nextjs-app/postcss.config.mjs
@@ -1,1 +1,10 @@
+/*
 export { default } from '@examples/design-system/postcss.config.mjs';
+*/
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: { '@tailwindcss/postcss': {} },
+};
+
+export default config;
