b6b9cfcbe83b215b5af4df1c6773c2f6b2d448a1
Sebastien Vanvelthem <belgattitude@gmail.com>
2025-04-29 14:21:43 +0200

    feat: to latest

--- examples/apps/nextjs-app/src/instrumentation.ts
+++ examples/apps/nextjs-app/src/instrumentation.ts
@@ -2,12 +2,16 @@ import type * as Sentry from '@sentry/nextjs';
 
 export async function register() {
   if (process.env.NEXT_PUBLIC_SENTRY_ENABLED === 'true') {
-    if (process.env.NEXT_RUNTIME === 'nodejs') {
-      await import('../sentry.server.config');
-    }
+    try {
+      if (process.env.NEXT_RUNTIME === 'nodejs') {
+        await import('../sentry.server.config');
+      }
 
-    if (process.env.NEXT_RUNTIME === 'edge') {
-      await import('../sentry.edge.config');
+      if (process.env.NEXT_RUNTIME === 'edge') {
+        await import('../sentry.edge.config');
+      }
+    } catch (error) {
+      console.error('[Instrumentation] Failed to register Sentry.', error);
     }
   }
 }
@@ -15,9 +19,16 @@ export async function register() {
 let captureRequestError: typeof Sentry.captureRequestError | undefined;
 
 if (process.env.NEXT_PUBLIC_SENTRY_ENABLED === 'true') {
-  captureRequestError = await import('@sentry/nextjs').then(
-    (mod) => mod.captureRequestError
-  );
+  try {
+    captureRequestError = await import('@sentry/nextjs').then(
+      (mod) => mod.captureRequestError
+    );
+  } catch (error) {
+    console.error(
+      '[Instrumentation] Failed to load Sentry captureRequestError function.',
+      error
+    );
+  }
 }
 
 export const onRequestError = captureRequestError;
