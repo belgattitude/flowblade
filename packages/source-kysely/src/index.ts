cc860824b2d5fe53264daf5cab7697c4b0aaba2f
Sebastien Vanvelthem <belgattitude@gmail.com>
2025-06-02 14:39:52 +0200

    feat: export KyselyMssqlDialectParams and KyselyMssqlPoolOptions types

--- packages/source-kysely/src/index.ts
+++ packages/source-kysely/src/index.ts
@@ -1,4 +1,8 @@
 export * from './datasource/kysely-datasource';
 export * from './helpers/datasource-helpers';
+export type {
+  KyselyMssqlDialectParams,
+  KyselyMssqlPoolOptions,
+} from './utils/create-kysely-mssql-dialect';
 export { createKyselyMssqlDialect } from './utils/create-kysely-mssql-dialect';
 export { TediousConnUtils } from './utils/tedious-conn-utils';
