6844d93430aec305d29a762c03a24f5dfb2359a6
Sebastien Vanvelthem <belgattitude@gmail.com>
2025-12-03 11:44:20 +0100

    feat: examples disambiguate raw vs query builder

--- examples/apps/nextjs-app/src/server/queries/currency/get-currency.query.ts
+++ examples/apps/nextjs-app/src/server/queries/currency/get-currency.query.ts
@@ -1,19 +1,12 @@
-import { sql } from 'kysely';
-
 import { dbKyselyMssql } from '@/server/config/db.kysely-mssql.config';
 
 type Params = {
   locale: string;
 };
 
-type Response = {
-  currency_id: number;
-  code: string;
-  symbol: string;
-  name: string;
-  namePlural: string;
-};
-
+/**
+ * Example for a kysely query builder query to get currencies with i18n support
+ */
 export const getCurrencyQuery = (params: Params) => {
   const { locale } = params;
   const { fn } = dbKyselyMssql;
@@ -38,15 +31,4 @@ export const getCurrencyQuery = (params: Params) => {
         .as('namePlural'),
     ])
     .execute();
-
-  return sql<Response>`
-      SELECT cu.id as currency_id,
-             cu.code,
-             cu.symbol,
-             COALESCE(cuI18n.name, cu.name) AS name,
-             COALESCE(cuI18n.name_plural, cu.name_plural) AS namePlural
-      FROM [common].[currency] AS cu
-      LEFT OUTER JOIN [common].[currency_i18n] AS cuI18n
-      ON cu.id = cui18n.currency_id and cuI18n.locale = ${locale}        
-  `;
 };
