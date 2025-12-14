1b90aa84ca2734e6d5dff90235c04cd027b19391
Sebastien Vanvelthem <belgattitude@gmail.com>
2025-12-14 14:09:11 +0100

    feat: add ideas in examples

--- examples/shared/sqlduck/src/sql-duck.test.ts
+++ examples/shared/sqlduck/src/sql-duck.test.ts
@@ -6,51 +6,65 @@ import { createFakeRowsIterator } from '../tests/utils/create-fake-rows-iterator
 import { rowsToColumnsChunk } from '../tests/utils/rows-to-columns';
 import { SqlDuck } from './sql-duck';
 
+const testTimeout = 10_000;
+
 describe('Duckdb tests', () => {
-  describe('toDuckdb', () => {
-    it('should', async () => {
-      const conn = await createDuckdbTestMemoryDb();
-      await conn.run(
-        // `ATTACH ':memory:' AS memory_db (ACCESS_MODE 'READ_WRITE', COMPRESS 'true')`
-        `ATTACH ':memory:' AS memory_db (COMPRESS 'true')`
-      );
-      const _databases = await conn.runAndReadAll('SHOW DATABASES');
-      const test = await conn.runAndReadAll('SHOW TABLES FROM memory_db');
-      expect(test.getRowObjects()).toStrictEqual([]);
-      const sqlDuck = new SqlDuck({ conn });
+  describe(
+    'toDuckdb',
+    () => {
+      it('should', async () => {
+        const conn = await createDuckdbTestMemoryDb();
+        await conn.run(
+          // `ATTACH ':memory:' AS memory_db (ACCESS_MODE 'READ_WRITE', COMPRESS 'true')`
+          `ATTACH ':memory:' AS memory_db (COMPRESS 'true')`
+        );
+        const _databases = await conn.runAndReadAll('SHOW DATABASES');
+        const test = await conn.runAndReadAll('SHOW TABLES FROM memory_db');
+        expect(test.getRowObjects()).toStrictEqual([]);
+        const sqlDuck = new SqlDuck({ conn });
 
-      const userSchema = z.object({
-        id: z.number(),
-        name: z.string(),
-        email: z.email(),
-        created_at: z.date(),
-      });
-      const limit = 1_000_000;
+        const userSchema = z.object({
+          id: z.number(),
+          name: z.string(),
+          email: z.email(),
+          created_at: z.date(),
+        });
+        const limit = 3_000_000;
 
-      const rowGen = createFakeRowsIterator({
-        count: limit,
-        schema: userSchema,
-        factory: ({ faker }) => {
+        const now = new Date();
+        const rowGen = createFakeRowsIterator({
+          count: limit,
+          schema: userSchema,
+          factory: ({ faker, rowIdx }) => {
+            return {
+              id: rowIdx,
+              name: `name-${rowIdx}`,
+              email: `email-${rowIdx}@example.com`,
+              created_at: now,
+            };
+            /*
           return {
             id: faker.number.int(),
             name: faker.person.fullName(),
             email: faker.internet.email(),
             created_at: faker.date.recent(),
-          };
-        },
-      });
-      const chunkedCols = rowsToColumnsChunk(rowGen(), 2000);
-      const _inserted = await sqlDuck.toTable('memory_db.test', chunkedCols);
+          }; */
+          },
+        });
+        const chunkedCols = rowsToColumnsChunk(rowGen(), 2000);
+        const _inserted = await sqlDuck.toTable('memory_db.test', chunkedCols);
 
-      const query = await conn.runAndReadAll(
-        'SELECT count(*) as count_star from memory_db.test'
-      );
-      const data = query.getRowObjects();
-      expect(data).toStrictEqual([
-        {
-          count_star: BigInt(limit),
-        },
-      ]);
-    });
-  }, 10_000);
+        const query = await conn.runAndReadAll(
+          'SELECT count(*) as count_star from memory_db.test'
+        );
+        const data = query.getRowObjects();
+        expect(data).toStrictEqual([
+          {
+            count_star: BigInt(limit),
+          },
+        ]);
+      });
+    },
+    testTimeout
+  );
 });
--- examples/shared/sqlduck/tests/e2e/utils/create-duckdb-test-memory-db.ts
+++ examples/shared/sqlduck/tests/e2e/utils/create-duckdb-test-memory-db.ts
@@ -2,11 +2,20 @@ import * as os from 'node:os';
 
 import { type DuckDBConnection, DuckDBInstance } from '@duckdb/node-api';
 
-export const createDuckdbTestMemoryDb = async (): Promise<DuckDBConnection> => {
+export const createDuckdbTestMemoryDb = async (options?: {
+  access_mode?: 'READ_WRITE';
+  max_memory?: `${number}M`;
+  threads?: number;
+}): Promise<DuckDBConnection> => {
+  const {
+    access_mode = 'READ_WRITE',
+    max_memory = '64M',
+    threads = Math.max(1, Math.min(os.availableParallelism() - 1, 4)),
+  } = options ?? {};
   const instance = await DuckDBInstance.create(undefined, {
-    access_mode: 'READ_WRITE',
-    max_memory: '64MB',
-    threads: `${Math.max(1, Math.min(os.availableParallelism() - 1, 4))}`,
+    access_mode,
+    max_memory,
+    threads: String(threads),
   });
   return await instance.connect();
 };
