75ecfe2c7ba15db4e193154c30c70f9b237a66e1
Sebastien Vanvelthem <belgattitude@gmail.com>
2025-12-16 01:21:59 +0100

    feat: try out ideas

--- examples/shared/sqlduck/src/schema/zod-duckdb-schema.ts
+++ examples/shared/sqlduck/src/schema/zod-duckdb-schema.ts
@@ -4,7 +4,7 @@ export const getZodDuckDBSchema = <T extends ZodObject>(schema: T) => {
   const entries = schema.shape as Record<string, ZodObject>;
   const meta = [];
   for (const [key, fieldSchema] of Object.entries(entries)) {
-    meta.push({ key, duckdb: fieldSchema.meta() });
+    meta.push({ key, ...fieldSchema.meta() });
   }
   return meta;
 };
