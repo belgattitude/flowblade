42feca48d19fe97f796f54a5e916b2ab2ea52b2c
Sébastien Vanvelthem <belgattitude@users.noreply.github.com>
2025-12-18 12:31:56 +0100

    Sql formatter update UUID (#943)
    * chore: add rolldown to whitelist
    
    * fix(example): not null inversion logic
    
    * chore(dev-deps): all to latest
    
    * test(examples): add schema
    
    * chore(deps): update sql-formatter to 15.6.12
    
    * docs: add changeset
    
    * chore: add httpx/assert
    
    * chore: add httpx/assert
    
    * test(examples): properly test plain dates

---
+++ .changeset/dark-wolves-work.md
@@ -0,0 +1,5 @@
+---
+"@flowblade/sql-tag-format": patch
+---
+
+Support postgres 18 new uuid by updating sql-formatter to latest
--- examples/shared/sqlduck/src/table/get-table-create-from-zod.ts
+++ examples/shared/sqlduck/src/table/get-table-create-from-zod.ts
@@ -40,7 +40,7 @@ export const getTableCreateFromZod = <T extends ZodObject>(
     }
     if (primaryKey === true) {
       colDDL.push(`PRIMARY KEY`);
-    } else if (nullable !== false) {
+    } else if (nullable !== true) {
       colDDL.push('NOT NULL');
     }
     columns.push(colDDL.join(' '));
