6d5e115e433ac30ad9eb0b942201d5f4aff7657d
Sebastien Vanvelthem <belgattitude@gmail.com>
2026-01-12 13:14:25 +0100

    feat: export ToTableParams type

--- packages/sqlduck/src/index.ts
+++ packages/sqlduck/src/index.ts
@@ -1,4 +1,4 @@
-export type { SqlDuckParams } from './sql-duck';
+export type { SqlDuckParams, ToTableParams } from './sql-duck';
 export { SqlDuck } from './sql-duck';
 export { getTableCreateFromZod } from './table/get-table-create-from-zod';
 export { Table } from './table/table';
