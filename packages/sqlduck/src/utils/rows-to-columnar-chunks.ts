07dab56ce47032b399b000ae5c030a09978b72d6
Sebastien Vanvelthem <belgattitude@gmail.com>
2026-02-04 00:16:45 +0200

    feat: add rowToColumnarChunks

--- packages/sqlduck/src/utils/rows-to-columnar-chunks.ts
+++ packages/sqlduck/src/utils/rows-to-columnar-chunks.ts
@@ -1,14 +1,28 @@
 // type SupportedRowTypes = string | number | boolean | Date | bigint | null;
 type SupportedRowTypes = unknown;
 
+type IsUnique<
+  T extends readonly unknown[],
+  Acc extends readonly unknown[] = [],
+> = T extends readonly [infer First, ...infer Rest]
+  ? First extends Acc[number]
+    ? false
+    : IsUnique<Rest, readonly [...Acc, First]>
+  : true;
+
+type EnsureUniqueArray<T extends readonly unknown[]> =
+  IsUnique<T> extends true ? T : never;
+
 type RowsToColumnarChunksParams<
   TRow extends Record<string, SupportedRowTypes>,
 > = {
   rows: AsyncGenerator<TRow> | Generator<TRow> | AsyncIterableIterator<TRow>;
   chunkSize: number;
+  // pickKeys?: (keyof TRow)[];
+  pickKeys?: EnsureUniqueArray<readonly (keyof TRow)[]>;
 };
 
-type RecordToColumnar<T extends Record<string, unknown>> = {
+export type RecordToColumnar<T extends Record<string, unknown>> = {
   [K in keyof T]: T[K][];
 } & {};
 
@@ -17,21 +31,24 @@ type RecordToColumnar<T extends Record<string, unknown>> = {
  *
  * @example
  * ```typescript
- * type Row = {id:string, name:string};
+ * type Row = { id:string, name:string, email:string };
  *
  * async function* streamFromDb(): AsyncGenerator<Row> {
- *   yield {id:'1',name:'A'};
- *   yield {id:'2',name:'B'};
- *   yield {id:'3',name:'C'};
+ *   yield { id:'1', name:'A','email': 'a@b.com' };
+ *   yield { id:'2', name:'B','email': 'a@b.com' };
+ *   yield { id:'3', name:'C','email': 'a@b.com' };
  * }
  *
  * const chunkedDataframes = await rowsToColumnarChunks({
- *  rows:streamFromDb,
- *  chunkSize:2
+ *  rows: streamFromDb,
+ *  chunkSize: 2,
+ *  // Optional, will pick all keys if not specified
+ *  // if specified will pick the keys and maintain order
+ *  pickKeys: ['id','name'],
  * });
  *
  * const firstChunk = await chunkedDataframes.next();
- * // firstChunk.value === {id: ['1','2'], name: ['A','B']}
+ * // firstChunk.value === { id: ['1','2'], name: ['A','B'] }
  * ```
  */
 export async function* rowsToColumnarChunks<
