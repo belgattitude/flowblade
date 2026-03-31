b970ec7770b6fb100876fc9d2f5865e2fc144b43
Sebastien Vanvelthem <belgattitude@gmail.com>
2026-03-31 13:17:26 +0300

    refactor: make zod validator an external path

--- packages/sqlduck/.size-limit.ts
+++ packages/sqlduck/.size-limit.ts
@@ -5,13 +5,13 @@ const config = [
     name: 'Import * (ESM)',
     path: ['dist/index.mjs'],
     import: '*',
-    limit: '40Kb',
+    limit: '60Kb',
   },
   {
     name: 'Only { SqlDuck } (ESM)',
     path: ['dist/index.mjs'],
     import: '{ SqlDuck }',
-    limit: '30Kb',
+    limit: '50Kb',
   },
 ] satisfies SizeLimitConfig;
 
