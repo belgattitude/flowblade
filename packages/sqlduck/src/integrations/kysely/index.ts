2da170f9a249a3aea557bb0210e2d3fec29f99cb
Sebastien Vanvelthem <belgattitude@gmail.com>
2026-08-25 10:25:12 +0300

    feat: small linter improvements

--- packages/sqlduck/src/integrations/kysely/index.ts
+++ packages/sqlduck/src/integrations/kysely/index.ts
@@ -1,3 +1,2 @@
 export { compileDuckQuery } from "./compile-duck-query.ts";
 export { createDuckKysekyQueryBuilder } from "./create-duck-kysely-query-builder.ts";
-export { withMaterializedKyselyQuery } from "./with-materialized-kysely-query.ts";
